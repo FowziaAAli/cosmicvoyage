9b82908b155a2b0c9dc802f5d23cf810d553f526
FowziaAAli <152695706+FowziaAAli@users.noreply.github.com>
2023-12-08 15:33:21 -0500

    second try

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const nextPage = document.querySelector('.next-page');
 button.addEventListener('click', () => {
     console.log('Button clicked!'); // Debugging
     // Redirect to the next page (replace 'next-page.html' with the actual URL)
-    window.location.href = 'spacesite.html';
+    window.location.href = 'main.html';
 
     // Hide the entry page and show the next page with a fade effect
-    document.querySelector('.entry-page').style.opacity = '0';
+    document.querySelector('.enter').style.opacity = '0';
 
     // Use a timeout to ensure the transition starts after the entry page is hidden
     setTimeout(() => {
