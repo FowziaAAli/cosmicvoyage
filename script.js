0508c8445b023559feb4244282860623639d626a
FowziaAAli <152695706+FowziaAAli@users.noreply.github.com>
2023-12-08 19:58:30 -0500

    f

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ button.addEventListener('click', () => {
     window.location.href = 'main.html';
 
     // Hide the entry page and show the next page with a fade effect
-    document.querySelector('.enter').style.opacity = '0';
+    document.querySelector('.enter').style.opacity = '30';
 
     // Use a timeout to ensure the transition starts after the entry page is hidden
     setTimeout(() => {
         nextPage.style.display = 'flex';
-        nextPage.style.opacity = '1';
+        nextPage.style.opacity = '30';
     }, 100);
 });
